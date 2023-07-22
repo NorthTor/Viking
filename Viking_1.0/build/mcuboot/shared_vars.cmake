c8bb7fd0ff7e22c14c568455976a65819219ab41
m-gjerde <gjerde.magnus@gmail.com>
2023-07-22 18:14:30 +0200

    updated gitignore

--- Viking_1.0/build/mcuboot/shared_vars.cmake
+++
@@ -1,8 +0,0 @@
-add_custom_target(mcuboot_shared_property_target)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY KERNEL_HEX_NAME;zephyr.hex)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY ZEPHYR_BINARY_DIR;/Users/torkaufmanngjerde/Documents/Viking/Viking_1.0/build/mcuboot/zephyr)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY KERNEL_ELF_NAME;zephyr.elf)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY BUILD_BYPRODUCTS;/Users/torkaufmanngjerde/Documents/Viking/Viking_1.0/build/mcuboot/zephyr/zephyr.hex;/Users/torkaufmanngjerde/Documents/Viking/Viking_1.0/build/mcuboot/zephyr/zephyr.elf)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY SIGNATURE_KEY_FILE;root-ec-p256.pem)
-set_property(TARGET mcuboot_shared_property_target APPEND PROPERTY PM_YML_DEP_FILES;/opt/nordic/ncs/v2.3.0/bootloader/mcuboot/boot/zephyr/pm.yml)
-set_property(TARGET mcuboot_shared_property_target APPEND PROPERTY PM_YML_FILES;/Users/torkaufmanngjerde/Documents/Viking/Viking_1.0/build/mcuboot/zephyr/include/generated/pm.yml)
